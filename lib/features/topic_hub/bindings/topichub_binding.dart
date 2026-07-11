e83aa75a9546fc2a8ba92ae99be5a0d1df51a84e
Atharva Shinde <atharva.solves@gmail.com>
2026-07-11 09:46:56 +0530

    feat:configure dependency injection with Getx Bindings

---
+++ lib/features/topic_hub/bindings/topichub_binding.dart
@@ -0,0 +1,47 @@
+import 'package:get/get.dart';
+import 'package:getx_memo_app/core/network/dio_client.dart';
+import 'package:getx_memo_app/features/topic_hub/data/data_sources/topichub_remote_data_source.dart';
+import 'package:getx_memo_app/features/topic_hub/data/repositories_impl/topichib_repo_impl.dart';
+import 'package:getx_memo_app/features/topic_hub/domain/repositories/topichub_repo.dart';
+import 'package:getx_memo_app/features/topic_hub/domain/usecases/get_parent_categories_usecase.dart';
+import 'package:getx_memo_app/features/topic_hub/presentation/controllers/topichub_controller.dart';
+
+//WHY? :
+//     SoC :
+//     UI<- req ctr<UC<repo<RDS<DioClient (actually instances of them)
+//     waterfall of instance initialization
+//     UI needs to be dumb ,not init.
+
+//what : class for Dep Inj (Bindings)
+//       Ensure checklist is
+
+//HOW :
+//      ext Bindings
+//      @ovr dep()
+//      Get.lazyput<Type>(()=>impl);
+//      impl(req: Get.find())
+
+//lazyPut --> init only when screen is actually opened
+
+class TopichubBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<DioClient>(() => DioClient());
+
+    Get.lazyPut<TopichubRemoteDataSource>(
+      () => TopichubRemoteDataSourceImpl(dioClient: Get.find()),
+    );
+
+    Get.lazyPut<TopichubRepo>(
+      () => TopichibRepoImpl(topichubRemoteDataSource: Get.find()),
+    );
+
+    Get.lazyPut<GetParentCategoriesUsecase>(
+      () => GetParentCategoriesUsecase(topichubRepo: Get.find()),
+    );
+
+    Get.lazyPut<TopichubController>(
+      () => TopichubController(getParentCategoriesUsecase: Get.find()),
+    );
+  }
+}
